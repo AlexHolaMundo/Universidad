f015de216e31f362dd27a0b46773689e0badcfe9
AlexHolaMundo <walter.tipanluisa5714@utc.edu.ec>
2024-01-26 19:09:47 -0500

    feat register models in admin

--- Aplicaciones/MatriculasWATT/admin.py
+++ Aplicaciones/MatriculasWATT/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import carreraWATT, cursoWATT, asignaturaWATT
 # Register your models here.
+admin.site.register(carreraWATT)
+admin.site.register(cursoWATT)
+admin.site.register(asignaturaWATT)
